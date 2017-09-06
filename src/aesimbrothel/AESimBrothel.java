235d0838c7e4d65e73e8a44e7adf7455874c366d
John Miru <AEFictionDev@gmail.com>
2017-09-05 21:33:23 -0400

    Added License
    Added the GPL to the source.

--- src/aesimbrothel/AESimBrothel.java
+++ src/aesimbrothel/AESimBrothel.java
@@ -29,26 +29,9 @@ import javafx.stage.Stage;
  * @author John Miru <AEFictionDev@gmail.com>
  */
 public class AESimBrothel extends Application {
-    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        primaryStage.setTitle(makeTitle("Menu"));
         primaryStage.show();
     }
 
@@ -58,5 +41,8 @@ public class AESimBrothel extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
+    public String makeTitle(String subTitle) {
+        return "AESimBrothel - " + subTitle;
+    }
 }
